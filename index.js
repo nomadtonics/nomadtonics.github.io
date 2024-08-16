d95a28e5aa7ec1e149ed2acb08eca550cbe966be
nomadtonics <nomadtonics@gmail.com>
2024-08-16 10:52:55 +0530

    changes

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const root = document.getElementById('root')
 const poptitle = document.createElement('h1')
-poptitle.innerHTML = 'noicetom'.toUpperCase()
+poptitle.innerHTML = 'popeye'.toUpperCase()
+poptitle.addEventListener('click', () => {
+    window.location.href('/noicetom')
+})
 root.appendChild(poptitle)
