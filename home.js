a2a3956b00c26e74a4f7d36ad5f537be4eb84627
nomadtonics <nomadtonics@gmail.com>
2024-08-19 16:11:07 +0530

    chnages

--- home.js
+++ home.js
@@ -1,6 +1,8 @@
 export function navbar(){
     const nav = document.createElement('nav')
     nav.setAttribute('id', 'navbar')
+    nav.innerHTML = `
+    <ul><li>login</li><li>sign up</li></ul>`
     return nav
 }
 export function mainsection(){
