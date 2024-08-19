be7fc8e81ebff5ba7f6caf210b9c41d69323fcc8
nomadtonics <nomadtonics@gmail.com>
2024-08-19 16:30:51 +0530

    changes

--- nomad.js
+++ nomad.js
@@ -3,7 +3,7 @@ const title = document.querySelector('title')
 title.style.textTransform = 'uppercase'
 // root div of the app
 const nomad = document.getElementById('nomad')
-import { navbar } from './home'
-import { mainsection } from './home'
+import { navbar } from './home.js'
+import { mainsection } from './home.js'
 nomad.appendChild(navbar())
 nomad.appendChild(mainsection())
