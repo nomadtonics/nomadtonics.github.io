0d53cf0359a469121bd575296558818f79028997
nomadtonics <nomadtonics@gmail.com>
2024-08-19 16:16:09 +0530

    changes

--- home.js
+++ home.js
@@ -7,6 +7,14 @@ export function navbar(){
 }
 export function mainsection(){
     const sec = document.createAttribute('section')
+    sec.innerHTML = `
+    <h1 id='mainsecheading'>no mad</h1>
+    <p id='mainsecpara'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo, voluptatibus. Soluta illo officia repudiandae ipsam delectus reiciendis, modi, explicabo consequuntur voluptate nihil accusantium quaerat quibusdam! Nulla sequi quod molestias? Illum!</p>
+    <spa></span>`
     sec.setAttribute('id', 'mainsection')
     return sec
+}
+export function mainfooter(){
+    const foot = document.createElement('footer')
+    return foot
 }
