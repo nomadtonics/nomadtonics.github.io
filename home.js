8d92e04b2a664c779d3a9f2e408449893fb49e32
nomadtonics <nomadtonics@gmail.com>
2024-08-19 16:02:01 +0530

    chnages

---
+++ home.js
@@ -0,0 +1,10 @@
+export function navbar(){
+    const nav = document.createElement('nav')
+    nav.setAttribute('id', 'navbar')
+    return nav
+}
+export function mainsection(){
+    const sec = document.createAttribute('section')
+    sec.setAttribute('id', 'mainsection')
+    return sec
+}
--- nomad.js
+++ nomad.js
@@ -1,3 +1,9 @@
+// tab title
 const title = document.querySelector('title')
 title.style.textTransform = 'uppercase'
+// root div of the app
 const nomad = document.getElementById('nomad')
+import { navbar } from 'home.js'
+import { mainsection } from './home'
+nomad.appendChild(navbar)
+nomad.appendChild(mainsection)
