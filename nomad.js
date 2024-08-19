e71d3dab748f1ff4c0f039382bbd1b7f73e898c3
nomadtonics <nomadtonics@gmail.com>
2024-08-19 16:37:50 +0530

    changes

--- nomad.js
+++ nomad.js
@@ -3,31 +3,22 @@ const title = document.querySelector('title')
 title.style.textTransform = 'uppercase'
 // root div of the app
 const nomad = document.getElementById('nomad')
-function navbar(){
-    const nav = document.createElement('nav')
-    nav.setAttribute('id', 'navbar')
-    nav.innerHTML = `
-    <ul id='nav-item'>
-    <img src='./image' alt='no mad logo'>
-    </ul>
-    <ul id='nav-item'>
-    <li>login</li>
-    <li>sign up</li>
-    </ul>`
-    return nav
-}
-function mainsection(){
-    const sec = document.createAttribute('section')
-    sec.innerHTML = `
-    <h1 id='mainsecheading'>no mad</h1>
-    <p id='mainsecpara'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo, voluptatibus. Soluta illo officia repudiandae ipsam delectus reiciendis, modi, explicabo consequuntur voluptate nihil accusantium quaerat quibusdam! Nulla sequi quod molestias? Illum!</p>
-    <spa></span>`
-    sec.setAttribute('id', 'mainsection')
-    return sec
-}
-function mainfooter(){
-    constfoot = document.createElement('footer')
-    returnfoot
-}
+const navbar = document.createElement('nav')
+nav.setAttribute('id', 'navbar')
+nav.innerHTML = `
+<ul id='nav-item'>
+<img src='./image' alt='no mad logo'>
+</ul>
+<ul id='nav-item'>
+<li>login</li>
+<li>sign up</li>
+</ul>`
+const mainsection = document.createAttribute('section')
+sec.innerHTML = `
+<h1 id='mainsecheading'>no mad</h1>
+<p id='mainsecpara'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo, voluptatibus. Soluta illo officia repudiandae ipsam delectus reiciendis, modi, explicabo consequuntur voluptate nihil accusantium quaerat quibusdam! Nulla sequi quod molestias? Illum!</p>
+<spa></span>`
+sec.setAttribute('id', 'mainsection')
+const mainfooter = document.createElement('footer')
 nomad.appendChild(navbar())
 nomad.appendChild(mainsection())
