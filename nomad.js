e88d9a22dd1ba84ffd45dd7b3fffc1dc2737925d
nomadtonics <nomadtonics@gmail.com>
2024-08-19 17:48:48 +0530

    changes

--- nomad.js
+++ nomad.js
@@ -11,18 +11,18 @@ const navbar = document.createElement('nav')
 navbar.setAttribute('id', 'navbar')
 // content to be displayed
 navbar.innerHTML = `
-<ul id='nav-item'>
-<img src='./logo.png' alt='no mad logo' id='logo'>
+<ul id='logo-container'>
+<img src='./logo.png' alt='no mad logo' id='logo-image'>
 </ul>
-<ul id='nav-item'>
-<li>login</li>
-<li>sign up</li>
+<ul id='menu-items'>
+<li id='menu-item'>login</li>
+<li id='menu-item'>sign up</li>
 </ul>`
 const mainsection = document.createElement('section')
 mainsection.innerHTML = `
 <h1 id='mainsecheading'>no mad</h1>
 <p id='mainsecpara'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo, voluptatibus. Soluta illo officia repudiandae ipsam delectus reiciendis, modi, explicabo consequuntur voluptate nihil accusantium quaerat quibusdam! Nulla sequi quod molestias? Illum!</p>
-<spa></span>`
+<span id='small-text'></span>`
 mainsection.setAttribute('id', 'mainsection')
 const mainfooter = document.createElement('footer')
 document.body.appendChild(navbar)
