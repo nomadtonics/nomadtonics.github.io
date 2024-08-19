5f80ccc8c15f3f95fac8329bf8a1002d68d1a134
nomadtonics <nomadtonics@gmail.com>
2024-08-19 16:48:31 +0530

    changes

--- nomad.js
+++ nomad.js
@@ -3,8 +3,11 @@ const title = document.querySelector('title')
 title.style.textTransform = 'uppercase'
 // root div of the app
 const nomad = document.getElementById('nomad')
+// navigation bar for the app
 const navbar = document.createElement('nav')
+// set navigation bar id
 navbar.setAttribute('id', 'navbar')
+// content to be displayed
 navbar.innerHTML = `
 <ul id='nav-item'>
 <img src='./logo.png' alt='no mad logo'>
@@ -20,5 +23,7 @@ mainsection.innerHTML = `
 <spa></span>`
 mainsection.setAttribute('id', 'mainsection')
 const mainfooter = document.createElement('footer')
+console.log(navbar)
+console.log(mainsection)
 nomad.appendChild(navbar)
 nomad.appendChild(mainsection)
