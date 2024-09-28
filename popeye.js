60978457aff8c53de2e281adc06362131987f18f
nomadtonics <nomadtonics@gmail.com>
2024-09-28 19:33:37 +0530

    Update popeye.js

--- popeye.js
+++ popeye.js
@@ -1,3 +1,4 @@
+
 export default function ellow() {
   console.log('ello world')
 }
@@ -10,3 +11,28 @@ const response = await fetch("/post", {
   body: JSON.stringify({ username: "popeye" }),
   headers: myHeaders,
 });
+
+async function post(request) {
+  try {
+    const response = await fetch(request);
+    const result = await response.json();
+    console.log("Success:", result);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+const request1 = new Request("/post", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({ username: "popeye" }),
+});
+
+const request2 = new Request(request1, {
+  body: JSON.stringify({ username: "popeye" }),
+});
+
+post(request1);
+post(request2);
