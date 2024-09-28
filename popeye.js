d578791371cafb05a5b91959cbba5c0408baa169
nomadtonics <nomadtonics@gmail.com>
2024-09-28 19:31:39 +0530

    Update popeye.js

--- popeye.js
+++ popeye.js
@@ -1,3 +1,12 @@
 export default function ellow() {
   console.log('ello world')
 }
+
+const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+const response = await fetch("/post", {
+  method: "POST",
+  body: JSON.stringify({ username: "popeye" }),
+  headers: myHeaders,
+});
