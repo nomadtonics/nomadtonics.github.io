0686868780b2d76b7a3c79bef1ba21919ccc78ef
nomadtonics <nomadtonics@gmail.com>
2024-10-18 21:03:24 +0530

    Create landing.js
    adding landing page

---
+++ landing.js
@@ -0,0 +1,11 @@
+// landing page
+const landingPage = document.createElement('main')
+
+
+landingPage.addEventListener('load', () => {
+  this.style.backgroundColor = '#23262980';
+  this.style.color = '#232629';
+})
+
+
+body.appendChild(landingPage)
