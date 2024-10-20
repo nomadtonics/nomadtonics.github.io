4605d233af0d653625acada38e56e2299d700210
nomadtonics <nomadtonics@gmail.com>
2024-10-20 19:16:45 +0530

    changes

--- nomad.js
+++ nomad.js
@@ -16,7 +16,7 @@ const nomadheadingone = document.createElement('h1');
 nomadheadingone.setAttribute('id', 'nomad-heading-one');
 nomadheadingone.textContent = 'no mad';
 nomadheadingone.style.textTransform = 'uppercase';
-nomadheadingone.style.letterSpacing = '8rem';
+nomadheadingone.style.letterSpacing = '2.6em';
 nomadheadingone.style.fontSize = '269%';
 nomadheadingone.style.textAlign = 'center';
 // append heading to section element
@@ -34,7 +34,7 @@ const nomadheadingtwo = document.createElement('h1');
 nomadheadingtwo.setAttribute('id', 'nomad-heading-two');
 nomadheadingtwo.textContent = 'no mad';
 nomadheadingtwo.style.textTransform = 'uppercase';
-nomadheadingtwo.style.letterSpacing = '8rem';
+nomadheadingtwo.style.letterSpacing = '2.6em';
 nomadheadingtwo.style.fontSize = '269%';
 nomadheadingtwo.style.textAlign = 'center';
 // append heading to section element
