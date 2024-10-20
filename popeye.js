27c8eff47ab417ab4ef24aca1df52c20b33f0963
nomadtonics <nomadtonics@gmail.com>
2024-10-20 17:38:52 +0530

    debugging

--- popeye.js
+++ popeye.js
@@ -6,33 +6,33 @@ export default function ellow() {
 const myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
 
-const response = await fetch("/post", {
-  method: "POST",
-  body: JSON.stringify({ username: "popeye" }),
-  headers: myHeaders,
-});
+// const response = await fetch("/post", {
+//   method: "POST",
+//   body: JSON.stringify({ username: "popeye" }),
+//   headers: myHeaders,
+// });
 
-async function post(request) {
-  try {
-    const response = await fetch(request);
-    const result = await response.json();
-    console.log("Success:", result);
-  } catch (error) {
-    console.error("Error:", error);
-  }
-}
+// async function post(request) {
+//   try {
+//     const response = await fetch(request);
+//     const result = await response.json();
+//     console.log("Success:", result);
+//   } catch (error) {
+//     console.error("Error:", error);
+//   }
+// }
 
-const request1 = new Request("/post", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({ username: "popeye" }),
-});
+// const request1 = new Request("/post", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify({ username: "popeye" }),
+// });
 
-const request2 = new Request(request1, {
-  body: JSON.stringify({ username: "popeye" }),
-});
+// const request2 = new Request(request1, {
+//   body: JSON.stringify({ username: "popeye" }),
+// });
 
-post(request1);
-post(request2);
+// post(request1);
+// post(request2);
