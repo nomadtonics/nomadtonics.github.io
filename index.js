9cec84284835807efc9616050a73348d49e4ddfa
nomadtonics <nomadtonics@gmail.com>
2024-10-22 13:07:26 +0530

    changes

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+document.body.style.maxWidth = window.screen.availWidth;
+document.body.style.width = window.screen.availWidth;
+document.body.style.minWidth = window.screen.availWidth;
+document.body.style.maxHeight = window.screen.availHeight;
+document.body.style.height = window.screen.availHeight;
+document.body.style.minHeight = window.screen.availHeight;
 // create main element
 const nmmain = document.createElement('main');
 nmmain.textContent = 'nomadtonics'.toUpperCase();
 nmmain.setAttribute('id', 'nm-main');
-nmmain.style.maxWidth = window.screen.availWidth;
-nmmain.style.width = window.screen.availWidth;
-nmmain.style.minWidth = window.screen.availWidth;
-nmmain.style.maxHeight = window.screen.availHeight;
-nmmain.style.height = window.screen.availHeight;
-nmmain.style.minHeight = window.screen.availHeight;
+document.body.style.maxWidth = window.screen.availWidth;
+document.body.style.width = window.screen.availWidth;
+document.body.style.minWidth = window.screen.availWidth;
+document.body.style.maxHeight = window.screen.availHeight;
+document.body.style.height = window.screen.availHeight;
+document.body.style.minHeight = window.screen.availHeight;
 nmmain.style.backgroundColor = '#002611';
 
 // create a div element to display 
