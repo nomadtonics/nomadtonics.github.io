4209d2914335d579561401f7fcd07e631a842c41
nomadtonics <nomadtonics@gmail.com>
2024-10-22 13:34:39 +0530

    changes

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ nomadtitle.textContent = nomadtitle.textContent.toUpperCase();
 const nomadheading = document.createElement('h1');
 nomadheading.textContent = nomadtitle.textContent;
 nomadheading.style.fontSize = '300%';
-nomadheading.style.textShadow = '0 0 3em #999900';
+nomadheading.style.textShadow = '0 0 0.26em #999900';
 // append children to the body
 document.body.onload = function() {
     document.body.appendChild(nmmain);
