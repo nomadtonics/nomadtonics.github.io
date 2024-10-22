20ad143ed512168273751776a79634a6175569c0
nomadtonics <nomadtonics@gmail.com>
2024-10-22 12:52:21 +0530

    changes

---
+++ index.js
@@ -0,0 +1,9 @@
+const nmmain = document.createElement('main');
+nmmain.setAttribute('id', 'nm-main');
+nmmain.style.maxWidth = window.screen.availWidth;
+nmmain.style.width = window.screen.availWidth;
+nmmain.style.minWidth = window.screen.availWidth;
+nmmain.style.maxHeight = window.screen.availHeight;
+nmmain.style.height = window.screen.availHeight;
+nmmain.style.minHeight = window.screen.availHeight;
+document.documentElement.appendChild(nmmain);
