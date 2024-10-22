eee512197aff5bb94b1b83c37afc96c2be6f9ff7
nomadtonics <nomadtonics@gmail.com>
2024-10-22 13:39:27 +0530

    changes

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ const nmmain = document.createElement('main');
 //nmmain.style.fontSize = '300%';
 nmmain.style.alignSelf = 'center';
 nmmain.style.justifySelf = 'center';
-nmmain.style.maxWidth = window.screen.availWidth;
-nmmain.style.width = window.screen.availWidth;
-nmmain.style.minWidth = window.screen.availWidth;
-nmmain.style.maxHeight = window.screen.availHeight;
-nmmain.style.height = window.screen.availHeight;
-nmmain.style.minHeight = window.screen.availHeight;
+nmmain.style.maxWidth = 'inherit';
+nmmain.style.width = 'inherit';
+nmmain.style.minWidth = 'inherit';
+nmmain.style.maxHeight = 'inherit';
+nmmain.style.height = 'inherit';
+nmmain.style.minHeight = 'inherit';
 // nmmain.style.backgroundColor = '#002611';
 
 // create a div element to display 
@@ -31,7 +31,8 @@ nomadtitle.textContent = nomadtitle.textContent.toUpperCase();
 const nomadheading = document.createElement('h1');
 nomadheading.textContent = nomadtitle.textContent;
 nomadheading.style.fontSize = '300%';
-nomadheading.style.textShadow = '0 0 0.26em #999900';
+nomadheading.style.textShadow = '0 0 0.26em #290400';
+nomadheading.style.color = '#290400';
 // append children to the body
 document.body.onload = function() {
     document.body.appendChild(nmmain);
