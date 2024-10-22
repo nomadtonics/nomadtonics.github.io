45d8eb90e5d3878f9a96e068f5ddc313a85dabd7
nomadtonics <nomadtonics@gmail.com>
2024-10-22 12:59:03 +0530

    changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// create main element
 const nmmain = document.createElement('main');
 nmmain.setAttribute('id', 'nm-main');
 nmmain.style.maxWidth = window.screen.availWidth;
@@ -6,4 +7,17 @@ nmmain.style.minWidth = window.screen.availWidth;
 nmmain.style.maxHeight = window.screen.availHeight;
 nmmain.style.height = window.screen.availHeight;
 nmmain.style.minHeight = window.screen.availHeight;
-document.body.appendChild(nmmain);
+nmmain.style.backgroundColor = '#002611';
+
+// create a div element to display 
+const landingSection = document.createElement('section');
+landingSection.textContent = 'welcome to nomadtonics!';
+//landingSection.setAttribute('width', 'inherit');
+// landingSection.style.
+
+// append children to the body
+document.body.onload = function() {
+    document.body.appendChild(nmmain);
+    document.body.appendChild(landingSection);
+
+}
