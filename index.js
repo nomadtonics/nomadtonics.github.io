5b3650c4d521ada1dbe07a1b5ad788b9628b309d
nomadtonics <nomadtonics@gmail.com>
2024-10-22 12:14:05 +0530

    complete js adaption

---
+++ index.js
@@ -0,0 +1,7 @@
+document.documentElement.style.maxWidth = window.screen.availWidth;
+document.documentElement.style.width = window.screen.availWidth;
+document.documentElement.style.minWidth = window.screen.availWidth;
+document.documentElement.style.maxHeight = window.screen.availHeight;
+document.documentElement.style.height = window.screen.availHeight;
+document.documentElement.style.minHeight = window.screen.availHeight;
+document.documentElement.style.backgroundColor = `#002611`;
