7151d3656b841b66a8886e18bcf6e4a85f17d44f
nomadtonics <nomadtonics@gmail.com>
2025-05-10 22:21:25 +0530

    add div to footer

--- js/nomad.js
+++
@@ -1,6 +0,0 @@
-export default function nomadtemplate(templatename){
-    const newelement = document.createElement(templatename);
-    newelement.style.backgroundColor = '#000';
-    newelement.style.color = '#fff';
-    return newelement;
-}
