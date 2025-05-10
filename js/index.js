edecd98d4d8fe4a09199d41524573f05969a5bb1
nomadtonics <nomadtonics@gmail.com>
2025-05-10 20:56:00 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,6 +3,15 @@ import nomadtemplate from './nomad.js';
 const nomadmain = document.getElementId("nomad-main");
 nomadmain.textContent = "welcome to nomads".toUpperCase();
 
+const logincard = document.createElement("div");
+const usernameInput = document.createElement("input");
+const passkeyInput = document.createElement("input");
+const loginbtn = document.createElement("input");
+
+logincard.appendChild(usernameInput);
+logincard.appendChild(passkeyInput);
+logincard.appendChild(loginbtn);
+
 const logo = document.createElement("div");
 logo.setAttribute("id", "svgContainer");
 
@@ -39,3 +48,6 @@ function createSVG() {
 
 // Call the function when the page loads
 window.onload = createSVG;
+
+nomadmain.appendChild(logo);
+nomadmain.appendChild(logincard);
