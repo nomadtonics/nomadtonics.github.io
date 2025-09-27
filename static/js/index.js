970f38ff844cadd4df3ab34e8271dfa3683f8fe9
nomadtonics <nomadtonics@gmail.com>
2025-09-27 22:56:01 +0530

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -3,3 +3,9 @@ const body = document.body;
 const nomad = document.getElementById("nomadtonics");
 
 nomad.textContent = "nomatonics";
+
+nomad.addEventListener("load", () => {
+    nomad.style.width = nomad.style.width * 2;
+    nomad.style.textAlign = "center";
+    nomad.style.padding = "2rem";
+});
